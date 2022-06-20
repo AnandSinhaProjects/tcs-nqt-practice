b55725128dbdc1e317a85ad05bd2ee89b9b00a66
Anand Sinha <anands.projects22@gmail.com>
2022-06-20 18:35:12 +0530

    New Question Added

---
+++ reverse_string.py
@@ -0,0 +1,33 @@
+'''
+
+Reverse a String
+Basic Accuracy: 56.22% Submissions: 100k+ Points: 1
+
+You are given a string s. You need to reverse the string.
+
+Example 1:
+
+Input:
+s = Geeks
+Output: skeeG
+
+Example 2:
+
+Input:
+s = for
+Output: rof
+
+Your Task:
+
+You only need to complete the function reverseWord() that takes s as parameter and returns the reversed string.
+
+Expected Time Complexity: O(|S|).
+Expected Auxiliary Space: O(1).
+
+Constraints:
+1 <= |s| <= 10000
+'''
+
+a = input()
+a = a[::-1]
+print(a)
